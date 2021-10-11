6317c8f49ef9fb24f4782245356db530f3bd0538
KYGAS <45013496+KYGAS@users.noreply.github.com>
2021-10-11 14:56:13 +0200

    sorry lili... hate taldek more now ^~-
    ?should work now?

--- index.js
+++ index.js
@@ -11,14 +11,42 @@ module.exports = function Tracker(mod) {
 	
 	
 	const command = mod.command;
-	let enabled = true;
 	let magicalPierce = 0, magicalIgnore = 0, magicPercent = 0;
 	let physicalPierce = 0, physicalIgnore = 0, physicalPercent = 0;
-	let originalDefense = {};
+	let abnormalModifiers = {};
+	let refreshCheck = {};
+	let predictedResistance = {};
 	
-	command.add('resistances', ()=>{
-		enabled = !enabled;
-		command.message('Showing True Resistance? ' + enabled);
+	command.add('resistances', (mode)=>{
+		if(!mode){
+			mod.settings.enabled = !mod.settings.enabled;
+			command.message('Showing True Resistance? ' + mod.settings.enabled);
+		}else{
+			switch(mode){
+				case '1':
+				case "accurate":
+					mod.settings.mode = 'accurate';
+					command.message("Using accurate mode for resistances.");
+					break;
+				case '2':
+				case "prediction":
+					mod.settings.mode = 'prediciton';
+					command.message("Using prediciton mode for resistances.");
+					break;
+				case '3':
+				case "hardcoded":
+					console.log("Hardcoded mode not supported. Switching to prediction mode.");
+					command.message("Hardcoded mode not supported. Switching to prediction mode.");
+					command.exec('resistances prediciton');
+					break;
+				default:
+					command.message("Currently existing modes :");
+					command.message("1) Accurate");
+					command.message("2) Prediction");
+					command.message("3) Hardcoded");
+					break;
+			}
+		}
 	})
 	
 	mod.hook('S_PLAYER_STAT_UPDATE', 17, {filter : { fake: null } }, (event) => {
@@ -39,19 +67,204 @@ module.exports = function Tracker(mod) {
 	})
 	
 	mod.hook('S_LOAD_TOPO', 'raw', ()=>{
-		originalDefense = {};
+		abnormalModifiers = {};
 	})
 	
-	mod.hook('S_NPC_RESISTANCE', 1, {filter : { fake: null } }, (event) => {
-		if(!enabled) return;
-		if(!originalDefense[event.gameId])
-			originalDefense[event.gameId] = {
-				physicalResistance : event.physicalResistance,
-				magicalResistance : event.magicalResistance
+	mod.hook('S_ABNORMALITY_BEGIN', 5, {filter : { fake: null } }, (event)=>{
+		for ( let x in event.values ) {
+			if(	event.values[x].type == 1003 ){
+				// pres
+				if(!abnormalModifiers[event.target]){
+					abnormalModifiers[event.target] = {};
+				}
+				if(!abnormalModifiers[event.target][event.id]){
+					abnormalModifiers[event.target][event.id] = {};
+				}
+				abnormalModifiers[event.target][event.id]['physicalResistance'] = Math.floor( event.values[x].value );
+				refreshCheck[event.id] = true;
 			}
+			if(	event.values[x].type == 1004 ){
+				// mres
+				if(!abnormalModifiers[event.target]){
+					abnormalModifiers[event.target] = {};
+				}
+				if(!abnormalModifiers[event.target][event.id]){
+					abnormalModifiers[event.target][event.id] = {};
+				}
+				abnormalModifiers[event.target][event.id]['magicalResistance'] = Math.floor ( event.values[x].value );
+				refreshCheck[event.id] = true;
+			}
+			
+		}
+		if(event.id == 18852){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				physicalResistance : -5000,
+				magicalResistance : -5000
+			}
+		}
+		if(event.id == 18824){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				physicalResistance : -94 * event.stacks,
+				magicalResistance : -94 * event.stacks
+			}
+		}
+		if(event.id == 99003900){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				magicalResistance : 10000 * event.stacks
+			}
+		}
+		if(event.id == 99003800){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				physicalResistance : 10000 * event.stacks
+			}
+		}
+	})
+	
+	mod.hook('S_ABNORMALITY_REFRESH', 2, {filter : { fake: null } }, (event)=>{
+		if(event.id == 18824){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				physicalResistance : -94 * event.stacks,
+				magicalResistance : -94 * event.stacks
+			}
+		}
+		if(event.id == 99003900){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				magicalResistance : 10000 * event.stacks
+			}
+		}
+		if(event.id == 99003800){
+			if(!abnormalModifiers[event.target]){
+				abnormalModifiers[event.target] = {};
+			}
+			if(!abnormalModifiers[event.target][event.id]){
+				abnormalModifiers[event.target][event.id] = {};
+			}
+			abnormalModifiers[event.target][event.id] = {
+				physicalResistance : 10000 * event.stacks
+			}
+		}
+		if(mod.settings.mode == 'accurate' && refreshCheck[event.id]){
+			mod.send('C_REQUEST_ABNORMALITY_TOOLTIP_VALUE', 1, {
+				gameId : event.target,
+				id : event.id
+			})
+		}
+	})
+	
+	mod.hook('S_ABNORMALITY_END', 1, {filter : { fake: null } }, (event)=>{
+		if(abnormalModifiers[event.target])
+			if(abnormalModifiers[event.target][event.id]) delete abnormalModifiers[event.target][event.id];
+	})
+	
+	mod.hook('S_ABNORMALITY_TOOLTIP_VALUE', 1, {filter : { fake: null } }, (event)=>{
+		for ( let x in event.values ) {
+			if(	event.values[x].type == 1003 ){
+				// pres
+				if(!abnormalModifiers[event.gameId]){
+					abnormalModifiers[event.gameId] = {};
+				}
+				if(!abnormalModifiers[event.gameId][event.id]){
+					abnormalModifiers[event.gameId][event.id] = {};
+				}
+				abnormalModifiers[event.gameId][event.id]['physicalResistance'] = Math.floor( event.values[x].value );
+			}
+			if(	event.values[x].type == 1004 ){
+				// mres
+				if(!abnormalModifiers[event.gameId]){
+					abnormalModifiers[event.gameId] = {};
+				}
+				if(!abnormalModifiers[event.gameId][event.id]){
+					abnormalModifiers[event.gameId][event.id] = {};
+				}
+				abnormalModifiers[event.gameId][event.id]['magicalResistance'] = Math.floor ( event.values[x].value );
+			}
+		}
+	})
+	
+	mod.hook('S_NPC_RESISTANCE', 1, {filter : { fake: null } }, (event)=>{
+		if(!mod.settings.enabled || mod.settings.mode != 'accurate') return;
 		
-		event.magicalResistance -= (originalDefense[event.gameId].magicalResistance * magicPercent + magicalIgnore)
-		event.physicalResistance -= (originalDefense[event.gameId].physicalResistance * physicalPercent + physicalIgnore)
+		let physicalResistance = event.physicalResistance;
+		let magicalResistance = event.magicalResistance;
+		
+		if(abnormalModifiers[event.gameId]){
+			for( let id of Object.keys(abnormalModifiers[event.gameId]) ){
+				physicalResistance -= abnormalModifiers[event.gameId][id].physicalResistance || 0;
+				magicalResistance -= abnormalModifiers[event.gameId][id].magicalResistance || 0;
+			}
+		}
+		
+		event.magicalResistance -= (magicalResistance * Math.min(magicPercent, 0.8 ) + magicalIgnore)
+		event.physicalResistance -= (physicalResistance * Math.min(physicalPercent, 0.8 ) + physicalIgnore)
+		
+		event.magicalResistance = Math.max(-33333, event.magicalResistance);
+		event.physicalResistance = Math.max(-33333, event.physicalResistance);
+		
+		return true;
+	})
+	
+	mod.hook('S_NPC_RESISTANCE', 1, {filter : { fake: null } }, (event)=>{
+		if(!mod.settings.enabled || mod.settings.mode != 'prediciton') return;
+		
+		let physicalResistance = event.physicalResistance;
+		let magicalResistance = event.magicalResistance;
+		
+		if(abnormalModifiers[event.gameId]){
+			for( let id of Object.keys(abnormalModifiers[event.gameId]) ){
+				physicalResistance -= abnormalModifiers[event.gameId][id].physicalResistance || 0;
+				magicalResistance -= abnormalModifiers[event.gameId][id].magicalResistance || 0;
+			}
+		}
+		
+		if(!predictedResistance[event.gameId]) {
+			predictedResistance[event.gameId] = {}
+			predictedResistance[event.gameId] = {
+				physicalResistance :  physicalResistance,
+				magicalResistance : magicalResistance
+			}
+		}
+		
+		event.magicalResistance -= (predictedResistance[event.gameId].magicalResistance * Math.min(magicPercent, 0.8 ) + magicalIgnore)
+		event.physicalResistance -= (predictedResistance[event.gameId].physicalResistance * Math.min(physicalPercent, 0.8 ) + physicalIgnore)
+		
+		event.magicalResistance = Math.max(-33333, event.magicalResistance);
+		event.physicalResistance = Math.max(-33333, event.physicalResistance);
 		
 		return true;
 	})
---
+++ settings_migrator.js
@@ -0,0 +1,62 @@
+"use strict"
+
+const DefaultSettings = {
+	"enabled" : true, // ?_?
+	"mode" : "prediction", /*
+		Two working modes :
+			- accurate :
+				* tracks values of all abnormalities using WHITELISTED C_ packets
+				* accurate 99.99% of the time
+				* report bugs to KYGAS#8575
+			- prediction :
+				* tries to guess the base resistance of the boss
+				* 100% detection proof and safe ( unless you stream where people can see that your resistance numbers are modified )
+				* should be fairly accurate but might show 500-2000 higher resistance
+			- hardcoded :
+				* 100% accurate
+				* slow to update
+				* 100% detection proof and safe ( unless you stream where people can see that your resistance numbers are modified )
+				* will happen if someone spoonfeeds me data 
+	*/
+}
+
+module.exports = function MigrateSettings(from_ver, to_ver, settings) {
+    if (from_ver === undefined) {
+        // Migrate legacy config file
+        return Object.assign(Object.assign({}, DefaultSettings), settings);
+    } else if (from_ver === null) {
+        // No config file exists, use default settings
+        return DefaultSettings;
+    } else {
+        // Migrate from older version (using the new system) to latest one
+        if (from_ver + 1 < to_ver) {
+            // Recursively upgrade in one-version steps
+            settings = MigrateSettings(from_ver, from_ver + 1, settings);
+            return MigrateSettings(from_ver + 1, to_ver, settings);
+        }
+
+        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
+        // a switch for each version step that upgrades to the next version. This enables us to
+        // upgrade from any version to the latest version without additional effort!
+		
+        switch(to_ver)
+        {
+			// keep old settings, add new ones
+			default:
+				let oldsettings = settings
+				
+				settings = Object.assign(DefaultSettings, {});
+				
+				for(let option in oldsettings) {
+					if(settings[option]) {
+						settings[option] = oldsettings[option]
+					}
+				}
+
+				if(from_ver < to_ver) console.log('[Resistance Tracker] Your settings have been updated to version ' + to_ver + '. You can edit the new config file after the next relog.')
+				break;
+        }
+
+        return settings;
+    }
+}
